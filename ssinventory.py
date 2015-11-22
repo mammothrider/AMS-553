d9e20c7198cf595fe1b978c4ec3067e4c33e6dfc
Mammothrider <xushubin1992@gmail.com>
2015-11-22 14:13:23 +0900

    time2

--- ssinventory.py
+++ ssinventory.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 	order = 10000
 	price = 1
 	penalty = 10
-	checktime = 8
+	checktime = 2
 	time = 500
 	mincost = 0
 	minlarge = 0
@@ -86,12 +86,12 @@ if __name__ == '__main__':
 	ax = fig.add_subplot(111, projection = "3d")
 	ax.set_title("checktime = {}".format(checktime))
 	
-	testtime = 10
-	step = 10000
+	testtime = 20
+	step = 3000
 	for t in range(testtime):
 		print("testtime {}".format(t))
 		count = 0
-		for large in range(90000, 505000, step):
+		for large in range(90000, 303000, step):
 			for small in range(45000, large, step):
 				inv = cInventory(small, large, large, holdingcost, order, price, penalty, checktime, time)
 				cost = inv.simulator()
